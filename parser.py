e646f2b3853b3ffc9fa231fea409093d1f44c991
linikova <linikova@skbkontur.ru>
2016-11-24 20:42:22 +0500

    add encoding support

--- parser.py
+++ parser.py
@@ -6,6 +6,7 @@ import base64
 import sys
 import codecs
 import re
+from quopri import decodestring
 from datetime import datetime
 from contextlib import contextmanager
 
@@ -20,14 +21,29 @@ def setlocale(name):
         finally:
             locale.setlocale(locale.LC_ALL, saved)
 
-def get_subject(line):
-    prefix = '=?utf-8?B?'
+
+def get_subject(line, encoding, cipher):
+    prefix = '=\?' + encoding +'\?\w{1}\?'
     suffix = '?=?'
-    line = (re.search('Subject: (.*?) from', line)).group(1)
-    subject = line.replace(prefix, '').replace(suffix, '').replace(' ', '')
-    lenx = (len(subject) // 4)*4
-    decoded = base64.b64decode(subject[:lenx])
+    line = (re.search(r'Subject: (.*?) from', line)).group(1)
+    line = re.sub(re.compile(prefix), '', line)
+    subject = line.replace(suffix, '').replace(' ', '').replace('?', '')
+    if cipher == 'B':
+        lenx = (len(subject) // 4)*4
+        decoded = base64.b64decode(subject[:lenx])
+    if cipher == 'Q':
+        if re.search(r'=\w{0,1}$', subject):
+            subject = re.sub(r'=\w{0,1}$', '', subject)
+        decoded = decodestring(subject)
+    if encoding != 'utf-8':
+        decoded = decoded.decode(encoding)
+    else:
+        try:
+            decoded = unicode(decoded, "utf-8", 'ignore')
+        except:
+            print("ok")
     return decoded
+
 
 def get_date(line, max):
     logdate = re.search(r'^\w{3} \d{2} \d{2}:\d{2}:\d{2}', line).group(0)
@@ -38,21 +54,39 @@ def get_date(line, max):
     else:
         return False
 
-def main (in_files, out_file, pattern, max_date):
+
+def is_subject_get_encoding(line):
+    encoding  = ''
+    cipher = ''
+    if 'Subject:' in line:
+        mixed = re.search(r'Subject: =\?(.*?)\?(\w)\?', line)
+        if mixed:
+            encoding = mixed.group(1)
+            cipher = mixed.group(2)
+    return encoding, cipher
+
+
+def main(in_files, out_file, pattern, max_date):
     listing = glob(in_files)
     for file_log in listing:
         with open(file_log, mode='r') as f:
             for line in f:
-                if 'Subject: =?utf-8?B?' in line:
-                    subject = get_subject(line)
-                    if pattern in subject:
-                        if get_date(line, max_date):
-                            with open(out_file, 'a') as outfile:
-                                outfile.write("%s \n" % str(line))
+                encoding, cipher = is_subject_get_encoding(line)
+                if encoding and cipher:
+                    subject = get_subject(line, encoding, cipher)
+                    try:
+                        if pattern in subject:
+                            if get_date(line, max_date):
+                                with open(out_file, 'a') as outfile:
+                                    outfile.write("%s \n" % str(line))
+                    except:
+                        print(line)
 
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
-pattern = 'поступило требование'
-in_files= 'C:\\MINELOGS\\maillog*'
+pattern = 'получен'
+pattern = unicode(pattern, "utf-8")
+in_files = 'C:\\MINELOGS\\maillog*'
 out_file = 'C:\\MINELOGS\\matched_subject.log'
-max_date = 'Nov 24 09:00:00'
+max_date = 'Nov 24 01:00:00'
 main(in_files, out_file, pattern, max_date)
+
